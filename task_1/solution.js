8e8c2fce7ff5f065c7545755508a49820ebaf198
Anton Karavaev <karavaev.anton@gmail.com>
2023-01-11 21:46:00 +0300

    task_1 done

--- task_1/solution.js
+++ task_1/solution.js
@@ -2,6 +2,10 @@ function addInCartMessage(title, price) {
     let productName = title;
     let productPrice = price;
 
+    let message = productName + " за " + productPrice + " теперь в корзине!";
+
+    console.log(message);
+
     // Задание №1.1. Оповещение о добавлении в корзину
 
     // создайте переменную message
@@ -13,7 +17,6 @@ function addInCartMessage(title, price) {
     // добавьте в конец переменной message пробел
     // добавьте в конец переменной message текст 'теперь в корзине!'
     // выведите переменную message в консоль
-    // тестовый комментарий
 
     // Конец решения задания №1.1.
 
@@ -24,6 +27,9 @@ function addInCartCountChange(value) {
 
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
+
+    let newValue = oldValue;
+    newValue = newValue + 1;
 
     // создайте переменную newValue
     // присвойте ей значение oldValue
@@ -37,6 +43,9 @@ function addInCartCountChange(value) {
 function addInCartSumChange(sum, delta) {
     let oldSum = sum;
     let difference = delta;
+
+    let newSum = oldSum + difference;
+    let newSumText = `${newSum} ₽`;
 
     // Задание №1.3. Увеличение суммы в корзине
 
